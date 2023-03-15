873b0aed1fdda635f9809430804d92b849c53c79
Bruno Berrehuel <bruno@homepad.ch>
2023-03-15 23:15:58 +0100

    add lightspeed

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,8 +1,14 @@
 return {
   -- You can also add new plugins here as well:
   { "folke/todo-comments.nvim", config = function() require("todo-comments").setup {} end, event = "User AstroFile" },
-  { "kylechui/nvim-surround", config = function() require("nvim-surround").setup {} end, event = "User AstroFile" },
+  { "kylechui/nvim-surround",   config = function() require("nvim-surround").setup {} end, event = "User AstroFile" },
   "folke/tokyonight.nvim",
+  "NLKNguyen/papercolor-theme",
+  {
+    "ggandor/lightspeed.nvim",
+    config = function() require("lightspeed").setup {} end,
+    event = "User AstroFile",
+  },
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
   -- {
