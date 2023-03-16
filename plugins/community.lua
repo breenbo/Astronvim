9020f1e7c8e4664d3a1d1ab07b7ddaa4f0ec4308
Bruno Berrehuel <bruno@homepad.ch>
2023-03-16 11:37:39 +0100

    Add fugitive and community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,10 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
+  -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight" },
+  { import = "astrocommunity.editing-support/todo-comments-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,14 +1,13 @@
 return {
   -- You can also add new plugins here as well:
-  { "folke/todo-comments.nvim", config = function() require("todo-comments").setup {} end, event = "User AstroFile" },
-  { "kylechui/nvim-surround",   config = function() require("nvim-surround").setup {} end, event = "User AstroFile" },
-  "folke/tokyonight.nvim",
+  { "kylechui/nvim-surround", config = function() require("nvim-surround").setup {} end, event = "User AstroFile" },
   "NLKNguyen/papercolor-theme",
   {
     "ggandor/lightspeed.nvim",
     config = function() require("lightspeed").setup {} end,
     event = "User AstroFile",
   },
+  { "tpope/vim-fugitive", lazy = false },
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
   -- {
